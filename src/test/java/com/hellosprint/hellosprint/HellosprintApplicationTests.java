393c9058a7ae4743f8fbf50ed75cf62c194ccf1b
co16353sidak <sidakaulakh@gmail.com>
2020-03-03 15:25:37 +0530

    Initial Commit

---
+++ src/test/java/com/hellosprint/hellosprint/HellosprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellosprint.hellosprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellosprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
